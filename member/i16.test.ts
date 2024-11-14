6c07563fcfff45e1308b578cd3d933630a69e227
Alexander O'Mara <me@alexomara.com>
2024-11-13 23:33:37 -0500

    Better testing

--- member/i16.test.ts
+++ member/i16.test.ts
@@ -11,36 +11,50 @@ Deno.test('memberI16', () => {
 
 		declare public beta: number;
 
+		declare public gamma: number;
+
+		declare public delta: number;
+
 		public static override readonly BYTE_LENGTH: number = ((o) => {
 			o += memberI16(this, o, 'alpha');
 			o += memberI16(this, o, 'beta');
+			o += memberI16(this, o, 'gamma', true);
+			o += memberI16(this, o, 'delta', false);
 			return o;
 		})(super.BYTE_LENGTH);
 	}
 
-	assertEquals(Test.BYTE_LENGTH, 4);
+	assertEquals(Test.BYTE_LENGTH, 8);
 
 	const data = new Uint8Array(Test.BYTE_LENGTH);
 	const view = new DataView(data.buffer);
 	{
 		const test = new Test(data.buffer, 0, false);
 		test.alpha = 0x7fff;
-		test.beta = -1;
+		test.beta = -2;
+		test.gamma = -3;
+		test.delta = -4;
 
 		assertEquals(test.alpha, 0x7fff);
-		assertEquals(test.beta, -1);
+		assertEquals(test.beta, -2);
 		assertEquals(view.getInt16(0, false), 0x7fff);
-		assertEquals(view.getInt16(2, false), -1);
+		assertEquals(view.getInt16(2, false), -2);
+		assertEquals(view.getInt16(4, true), -3);
+		assertEquals(view.getInt16(6, false), -4);
 	}
 	{
 		const test = new Test(data.buffer, 0, true);
 		test.alpha = 0x7fff;
-		test.beta = -1;
+		test.beta = -2;
+		test.gamma = -3;
+		test.delta = -4;
 
 		assertEquals(test.alpha, 0x7fff);
-		assertEquals(test.beta, -1);
+		assertEquals(test.beta, -2);
 		assertEquals(view.getInt16(0, true), 0x7fff);
-		assertEquals(view.getInt16(2, true), -1);
+		assertEquals(view.getInt16(2, true), -2);
+		assertEquals(view.getInt16(4, true), -3);
+		assertEquals(view.getInt16(6, false), -4);
 	}
 });
 
@@ -52,35 +66,49 @@ Deno.test('memberU16', () => {
 
 		declare public beta: number;
 
+		declare public gamma: number;
+
+		declare public delta: number;
+
 		public static override readonly BYTE_LENGTH: number = ((o) => {
 			o += memberU16(this, o, 'alpha');
 			o += memberU16(this, o, 'beta');
+			o += memberI16(this, o, 'gamma', true);
+			o += memberI16(this, o, 'delta', false);
 			return o;
 		})(super.BYTE_LENGTH);
 	}
 
-	assertEquals(Test.BYTE_LENGTH, 4);
+	assertEquals(Test.BYTE_LENGTH, 8);
 
 	const data = new Uint8Array(Test.BYTE_LENGTH);
 	const view = new DataView(data.buffer);
 	{
 		const test = new Test(data.buffer, 0, false);
 		test.alpha = 0x7fff;
-		test.beta = 0xFFFF;
+		test.beta = 0xfffe;
+		test.gamma = 0xfffd;
+		test.delta = 0xfffc;
 
 		assertEquals(test.alpha, 0x7fff);
-		assertEquals(test.beta, 0xFFFF);
+		assertEquals(test.beta, 0xfffe);
 		assertEquals(view.getUint16(0, false), 0x7fff);
-		assertEquals(view.getUint16(2, false), 0xFFFF);
+		assertEquals(view.getUint16(2, false), 0xfffe);
+		assertEquals(view.getUint16(4, true), 0xfffd);
+		assertEquals(view.getUint16(6, false), 0xfffc);
 	}
 	{
 		const test = new Test(data.buffer, 0, true);
 		test.alpha = 0x7fff;
-		test.beta = 0xFFFF;
+		test.beta = 0xfffe;
+		test.gamma = 0xfffd;
+		test.delta = 0xfffc;
 
 		assertEquals(test.alpha, 0x7fff);
-		assertEquals(test.beta, 0xFFFF);
+		assertEquals(test.beta, 0xfffe);
 		assertEquals(view.getUint16(0, true), 0x7fff);
-		assertEquals(view.getUint16(2, true), 0xFFFF);
+		assertEquals(view.getUint16(2, true), 0xfffe);
+		assertEquals(view.getUint16(4, true), 0xfffd);
+		assertEquals(view.getUint16(6, false), 0xfffc);
 	}
 });
