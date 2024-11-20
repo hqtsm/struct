947459cc249b68bd8dea0d4b671408ccaf7db144
Alexander O'Mara <me@alexomara.com>
2024-11-19 21:18:27 -0500

    Test member behavior directly

---
+++ member.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals, assertNotStrictEquals } from '@std/assert';
+
+import { Struct } from './struct.ts';
+import { member } from './member.ts';
+
+Deno.test('member', () => {
+	class Test extends Struct {
+		declare public readonly ['constructor']: typeof Test;
+
+		public static override readonly BYTE_LENGTH: number = ((o) => {
+			o += 16;
+			o += member(this, 'unk' as never, o, 32, false, 'unk32');
+			return o;
+		})(super.BYTE_LENGTH);
+	}
+
+	assertNotStrictEquals(Test.MEMBERS, Struct.MEMBERS);
+	assertEquals(Test.MEMBERS.unk.byteOffset, 16);
+	assertEquals(Test.MEMBERS.unk.byteLength, 32);
+	assertEquals(Test.MEMBERS.unk.littleEndian, false);
+	assertEquals(Test.MEMBERS.unk.Type, 'unk32');
+});
