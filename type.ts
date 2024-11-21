5c481f186b24f41760da6760a6fa647ff9080edf
Alexander O'Mara <me@alexomara.com>
2024-11-20 22:51:20 -0500

    Unused types

--- type.ts
+++ type.ts
@@ -1,26 +1,6 @@
 import type { Struct } from './struct.ts';
 
 /**
- * If A equals B then C else D.
- */
-export type IfTypesEqual<A, B, C, D> = (<T>() => T extends A ? 1 : 2) extends
-	<T>() => T extends B ? 1 : 2 ? C : D;
-
-/**
- * Readonly keys for a given type.
- */
-export type ReadonlyKeyof<T> = NonNullable<
-	{
-		[K in keyof T]: IfTypesEqual<
-			{ [L in K]: T[K] },
-			{ readonly [L in K]: T[K] },
-			K,
-			never
-		>;
-	}[keyof T]
->;
-
-/**
  * Keys of a type that another type extends.
  */
 export type KeyofExtends<T, E> = {
@@ -28,31 +8,15 @@ export type KeyofExtends<T, E> = {
 }[keyof T];
 
 /**
- * Readonly keys of a type that another type extends.
- */
-export type ReadonlyKeyofExtends<T, E> = {
-	[K in ReadonlyKeyof<T>]: E extends T[K] ? K : never;
-}[ReadonlyKeyof<T>];
-
-/**
  * Possible members of a struct.
  */
-// deno-lint-ignore no-explicit-any
-export type Members<S extends typeof Struct> = MembersExtends<S, any>;
+export type Members<S extends typeof Struct> = Exclude<keyof S, keyof Struct>;
 
 /**
  * Possible members of a struct that another type extends.
  */
 export type MembersExtends<S extends typeof Struct, T> = Exclude<
 	KeyofExtends<S['prototype'], T>,
-	keyof Struct
->;
-
-/**
- * Possible readonly members of a struct that another type extends.
- */
-export type ReadonlyMembersExtends<S extends typeof Struct, T> = Exclude<
-	ReadonlyKeyofExtends<S['prototype'], T>,
 	keyof Struct
 >;
 
