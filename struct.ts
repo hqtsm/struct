553be2e6102e63a846dcd314d014ffbfb946e87d
Alexander O'Mara <me@alexomara.com>
2024-11-22 15:25:00 -0500

    Shorter message

--- struct.ts
+++ struct.ts
@@ -124,7 +124,7 @@ export class Struct implements ArrayBufferView {
 		littleEndian: boolean | null = null,
 	) {
 		if ((byteOffset |= 0) < 0) {
-			throw new RangeError(`Negative byte offset: ${byteOffset}`);
+			throw new RangeError(`Negative offset: ${byteOffset}`);
 		}
 		this.#buffer = buffer;
 		this.#byteOffset = byteOffset;
