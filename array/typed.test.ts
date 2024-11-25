9a0d42cfff4fd29c2fed79e552bb6ef33f8bf9b0
Alexander O'Mara <me@alexomara.com>
2024-11-24 20:12:29 -0500

    Created abstract ArrayTyped class

---
+++ array/typed.test.ts
@@ -0,0 +1,159 @@
+import { assertEquals } from '@std/assert';
+import { ArrayTyped } from './typed.ts';
+
+// Properties to ensure matching TypedArray behavior.
+// Use real TypedArray to detect the expected bahavior.
+// Some properties:
+// - Map to an index (uint in range 0 to length-1).
+// - Get discarded (numeric non-uint and out-of-range).
+// - Map to a regular property/symbol (everything else).
+const properties = [
+	...new Set([
+		...[
+			0,
+			0.5,
+			1,
+			1.5,
+			2,
+			100,
+			Math.PI,
+			Number.EPSILON,
+			Infinity,
+			NaN,
+			Number.MAX_SAFE_INTEGER,
+			Number.MAX_VALUE,
+			Number.MIN_VALUE,
+			0n,
+			1n,
+			0xffffffffffffffffn,
+			'.1',
+			'.5',
+			'.9',
+			'0n',
+			'1n',
+			'0xffffffffffffffffn',
+			'',
+			'0x0',
+			'0x1',
+			'0b0',
+			'0b1',
+			[
+				[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+				[10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+				[20, 21],
+			].flat().map((i) => `1e${i}`),
+			'0.0000000000000001',
+			'0.0000000000000009',
+			'0.9999999999999999',
+			'1.0000000000000001',
+			'1.0000000000000009',
+			'1.9999999999999999',
+			'unknown',
+		].map((n) => [n, -n, `${n}`, `-${n}`, `+${n}`]).flat(),
+		...new Set(
+			[
+				true,
+				false,
+				null,
+				undefined,
+			].map((n) => [n, `${n}`, `-${n}`, `+${n}`]).flat(),
+		),
+		Symbol('symbol'),
+		{},
+		[],
+	]),
+];
+
+Deno.test('properties', () => {
+	class Test extends ArrayTyped<number> {
+		protected override [ArrayTyped.getter](_index: number): number {
+			return 0;
+		}
+
+		protected override [ArrayTyped.setter](
+			_index: number,
+			_value: number,
+		): void {
+		}
+	}
+
+	for (const p of properties) {
+		const spec = new Uint8Array([0, 1]);
+		const sop = Object.getOwnPropertyNames(spec).length;
+		const sos = Object.getOwnPropertySymbols(spec).length;
+		spec[p as number] = 2;
+		const sap = Object.getOwnPropertyNames(spec).length - sop;
+		const sas = Object.getOwnPropertySymbols(spec).length - sos;
+
+		const test = new Test(new ArrayBuffer(2), 0, 2);
+		const top = Object.getOwnPropertyNames(test).length;
+		const tos = Object.getOwnPropertySymbols(test).length;
+		test[p as number] = 2;
+		const tap = Object.getOwnPropertyNames(test).length - top;
+		const tas = Object.getOwnPropertySymbols(test).length - tos;
+
+		assertEquals(tap, sap, `[${String(p)}]: ${tap} != ${sap}`);
+		assertEquals(tas, sas, `[${String(p)}]: ${tas} != ${sas}`);
+	}
+});
+
+Deno.test('getter', () => {
+	class Test extends ArrayTyped<number> {
+		protected override [ArrayTyped.getter](index: number): number {
+			return index;
+		}
+
+		protected override [ArrayTyped.setter](
+			index: number,
+			value: number,
+		): void {
+			throw new Error(`Setter: ${index} = ${value}`);
+		}
+	}
+
+	for (const p of properties) {
+		const spec = new Uint8Array([0, 1]);
+		const expected = spec[p as number];
+
+		const test = new Test(new ArrayBuffer(2), 0, 2);
+		assertEquals(
+			test[p as number],
+			expected,
+			`[${String(p)}]: ${expected}`,
+		);
+	}
+});
+
+Deno.test('setter', () => {
+	let called: [number, number] | null;
+
+	class Test extends ArrayTyped<number> {
+		protected override [ArrayTyped.getter](index: number): number {
+			throw new Error(`Getter: ${index}`);
+		}
+
+		protected override [ArrayTyped.setter](
+			index: number,
+			value: number,
+		): void {
+			called = [index, value];
+		}
+	}
+
+	for (const p of properties) {
+		const spec = new Uint8Array([0, 1]);
+		spec[p as number] = 2;
+		let expected: [number, number] | null = null;
+		for (let i = 0; i < spec.length; i++) {
+			if (spec[i] === 2) {
+				expected = [i, 2];
+				break;
+			}
+		}
+
+		const test = new Test(new ArrayBuffer(2), 0, 2);
+		called = null as typeof called;
+		test[p as number] = 2;
+		assertEquals(called, expected, `[${String(p)}]: ${expected}`);
+	}
+});
---
+++ array/typed.ts
@@ -0,0 +1,182 @@
+import { LITTLE_ENDIAN } from '../endian.ts';
+import type { ArrayBufferReal, EndianBufferView } from '../type.ts';
+import { dataView } from '../util.ts';
+
+const handler: ProxyHandler<ArrayTyped<unknown>> = {
+	get(target, key): unknown | undefined {
+		if (key === '-0') {
+			return;
+		}
+		const index = +String(key);
+		if (key === '' + index) {
+			if (
+				index === (index | 0) &&
+				index >= 0 &&
+				index < target.length
+			) {
+				return target[getter](index);
+			}
+			return;
+		}
+		return (target as unknown as Record<typeof key, unknown>)[key];
+	},
+	set(target, key, value): boolean {
+		if (key === '-0') {
+			return true;
+		}
+		const index = +String(key);
+		if (key === '' + index) {
+			if (
+				index === (index | 0) &&
+				index >= 0 &&
+				index < target.length
+			) {
+				target[setter](index, value);
+			}
+			return true;
+		}
+		(target as unknown as Record<typeof key, unknown>)[key] = value;
+		return true;
+	},
+};
+
+const getter = Symbol('getter');
+
+const setter = Symbol('setter');
+
+/**
+ * ArrayTyped interface.
+ */
+export abstract class ArrayTyped<E> implements EndianBufferView {
+	/**
+	 * ArrayType class.
+	 */
+	declare public readonly ['constructor']: Omit<typeof ArrayTyped, 'new'>;
+
+	/**
+	 * Array elements.
+	 */
+	[index: number]: E;
+
+	/**
+	 * Buffer data.
+	 */
+	readonly #buffer: ArrayBufferLike;
+
+	/**
+	 * Byte offset into buffer.
+	 */
+	readonly #byteOffset: number;
+
+	/**
+	 * Array length (element count).
+	 */
+	readonly #length: number;
+
+	/**
+	 * Little endian, or big.
+	 */
+	readonly #littleEndian: boolean;
+
+	/**
+	 * ArrayTyped constructor.
+	 *
+	 * @param buffer Buffer data.
+	 * @param byteOffset Byte offset into buffer.
+	 * @param length Array length (element count).
+	 * @param littleEndian Host endian, little endian, big endian.
+	 */
+	constructor(
+		buffer: ArrayBufferReal,
+		byteOffset = 0,
+		length = 0,
+		littleEndian: boolean | null = null,
+	) {
+		dataView(this.#buffer = buffer);
+		if (byteOffset < 0) {
+			throw new RangeError(`Invalid offset: ${byteOffset}`);
+		}
+		if (length < 0) {
+			throw new RangeError(`Invalid length: ${length}`);
+		}
+		this.#byteOffset = byteOffset | 0;
+		this.#length = length | 0;
+		this.#littleEndian = !!(littleEndian ?? LITTLE_ENDIAN);
+		return new Proxy(this, handler as ProxyHandler<ArrayTyped<E>>);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public get buffer(): ArrayBuffer {
+		return this.#buffer;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public get byteLength(): number {
+		return this.length * this.bytesPerElement;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public get byteOffset(): number {
+		return this.#byteOffset;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public get littleEndian(): boolean {
+		return this.#littleEndian;
+	}
+
+	/**
+	 * Get array length (element count).
+	 *
+	 * @returns Array length.
+	 */
+	public get length(): number {
+		return this.#length;
+	}
+
+	/**
+	 * Get bytes per element.
+	 *
+	 * @returns Bytes length for element.
+	 */
+	public get bytesPerElement(): number {
+		return this.constructor.BYTES_PER_ELEMENT;
+	}
+
+	/**
+	 * Getter accessor.
+	 *
+	 * @returns Element value.
+	 */
+	protected abstract [getter](index: number): E;
+
+	/**
+	 * Setter accessor.
+	 *
+	 * @param value Element value.
+	 */
+	protected abstract [setter](index: number, value: E): void;
+
+	/**
+	 * Getter symbol.
+	 */
+	protected static readonly getter: typeof getter = getter;
+
+	/**
+	 * Setter symbol.
+	 */
+	protected static readonly setter: typeof setter = setter;
+
+	/**
+	 * Size of each array element.
+	 */
+	public static readonly BYTES_PER_ELEMENT: number;
+}
