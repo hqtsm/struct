82fb6cf4ccefcfa26406302e9921e613232bb479
Alexander O'Mara <me@alexomara.com>
2024-12-17 01:12:13 -0500

    Sanity check class constants

---
+++ mod.test.ts
@@ -0,0 +1,23 @@
+import { assertEquals, assertMatch } from '@std/assert';
+import * as mod from './mod.ts';
+
+const defaultClassProperties = new Set(Object.getOwnPropertyNames(class {}));
+
+Deno.test('class constants', () => {
+	for (const [k, v] of Object.entries(mod)) {
+		if (
+			typeof v !== 'function' ||
+			Object.getOwnPropertyDescriptor(v, 'prototype')?.writable
+		) {
+			continue;
+		}
+		for (const p of Object.getOwnPropertyNames(v)) {
+			if (defaultClassProperties.has(p)) {
+				continue;
+			}
+			const desc = Object.getOwnPropertyDescriptor(v, p);
+			assertMatch(p, /^[a-zA-Z][a-zA-Z0-9_]*$/, `${k}.${p}`);
+			assertEquals(desc!.writable ?? false, false, `${k}.${p}`);
+		}
+	}
+});
