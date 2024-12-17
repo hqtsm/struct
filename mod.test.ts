e0a2cddfe05ddeb635048be35ca5eb06d793afa9
Alexander O'Mara <me@alexomara.com>
2024-12-17 01:25:44 -0500

    Switched order

--- mod.test.ts
+++ mod.test.ts
@@ -3,6 +3,22 @@ import * as mod from './mod.ts';
 
 const defaultClassProperties = new Set(Object.getOwnPropertyNames(class {}));
 
+Deno.test('class Symbol.toStringTag', () => {
+	for (const [k, v] of Object.entries(mod)) {
+		if (
+			typeof v !== 'function' ||
+			Object.getOwnPropertyDescriptor(v, 'prototype')?.writable
+		) {
+			continue;
+		}
+		const desc = Object.getOwnPropertyDescriptor(
+			v.prototype,
+			Symbol.toStringTag,
+		);
+		assertEquals(desc?.value, k, k);
+	}
+});
+
 Deno.test('class constants', () => {
 	for (const [k, v] of Object.entries(mod)) {
 		if (
@@ -19,21 +35,5 @@ Deno.test('class constants', () => {
 			assertMatch(p, /^[a-zA-Z][a-zA-Z0-9_]*$/, `${k}.${p}`);
 			assertEquals(desc!.writable ?? false, false, `${k}.${p}`);
 		}
-	}
-});
-
-Deno.test('class Symbol.toStringTag', () => {
-	for (const [k, v] of Object.entries(mod)) {
-		if (
-			typeof v !== 'function' ||
-			Object.getOwnPropertyDescriptor(v, 'prototype')?.writable
-		) {
-			continue;
-		}
-		const desc = Object.getOwnPropertyDescriptor(
-			v.prototype,
-			Symbol.toStringTag,
-		);
-		assertEquals(desc?.value, k, k);
 	}
 });
