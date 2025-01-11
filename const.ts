9c6105834e5b51d2ec29ce9c2e757fe5fa5b9067
Alexander O'Mara <me@alexomara.com>
2025-01-11 00:45:28 -0500

    Added docs

--- const.ts
+++ const.ts
@@ -1,15 +1,30 @@
-// deno-lint-ignore-file ban-types
+/**
+ * @module
+ *
+ * Types for constant types.
+ */
+
 import type { Arr } from './arr.ts';
 import type { Ptr } from './ptr.ts';
 
+/**
+ * Constant type.
+ */
+// deno-lint-ignore ban-types
 export type Const<T> = T extends Function | RegExp | Date ? T
 	: T extends Arr<infer V> ? ConstArr<V> & Const<Omit<T, keyof Arr>>
 	: T extends Ptr<infer V> ? ConstPtr<V> & Const<Omit<T, keyof Ptr>>
-	: T extends {} ? { readonly [K in keyof T]: Const<T[K]> }
+	: T extends object ? { readonly [K in keyof T]: Const<T[K]> }
 	: T;
 
+/**
+ * Constant pointer.
+ */
 export interface ConstPtr<T = never>
-	extends Readonly<Omit<Ptr<Const<T>>, 'set'>> {}
+	extends Const<Omit<Ptr<Const<T>>, 'set'>> {}
 
+/**
+ * Constant array.
+ */
 export interface ConstArr<T = never>
-	extends Readonly<Omit<Arr<Const<T>>, 'set'>> {}
+	extends Const<Omit<Arr<Const<T>>, 'set'>> {}
